917754960caf95d87046cd23702d77da8ccc2dc2
STIKKIK <tik_thanawat@hotmail.com>
2018-09-10 18:49:37 +0700

    upload user api

---
+++ application/controllers/Users.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        // Load model
+        $this->load->model('user_model'); 
+        
+    }
+	public function get()
+    {
+        $data = $this->user_model->get_user();
+        echo json_encode($data);
+    }	
+    public function find_get($id)
+    {
+        $data = $this->user_model->get_user($id);
+		if(empty($data))
+        {
+            // set status header
+            $this->output->set_status_header(204);
+        }
+        else
+        {
+            echo json_encode($data);
+        }
+    }
+    public function insert()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $response = $this->user_model->insert_user($data);
+        echo json_encode($response);
+       
+    }
+    public function update()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $response = $this->user_model->update_user($data);
+        echo json_encode($response);
+    }
+    public function delete($id)
+    {
+        $response = $this->user_model->delete_user($id);
+        echo json_encode($response);
+	}
+}
---
+++ application/models/User_model.php
@@ -0,0 +1,75 @@
+<?php
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    
+    public function get_user($id = FALSE)
+    {
+        if ($id === FALSE)
+        {
+            $query = $this->db->get('user');
+            return $query->result();
+        }
+        $query = $this->db->get_where('user', array('user_id' => $id));
+        return $query->result();
+    }
+
+    public function insert_user($data)
+    {
+        // search name 
+        $query = $this->db->get_where('user', array('name' => $data['name']));
+        if ($query->num_rows() == 0) 
+        {
+            $this->db->insert('user', $data);
+            //$insert_id = $this->db->insert_id();
+            //return $insert_id;
+            //return 
+            if ($this->db->affected_rows() > 0) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }   
+        } 
+        else
+        {
+            return false;
+        }
+    }
+    
+    public function update_user($data)
+    {
+        $this->db->where('user_id', $data['user_id']);
+        $this->db->update('user', $data);
+
+        //return 
+        if ($this->db->affected_rows() > 0) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public function delete_user($id)
+    {
+        $this->db->delete('user', array('user_id' => $id));
+
+        //return 
+        if ($this->db->affected_rows() > 0) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
